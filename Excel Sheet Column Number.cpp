8cf6409744515b3781852119ef8025f9a407191d
khxkhx321 <khxkhx321@163.com>
2015-04-02 02:44:29 +0800

    Signed-off-by: khxkhx321 <khxkhx321@163.com>

---
+++ Excel Sheet Column Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int num = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            num = num * 26 + int(s[i] - 'A') + 1;
+        }
+        return num;
+    }
+};
